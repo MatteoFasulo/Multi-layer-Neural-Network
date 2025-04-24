0041870a295e93f0d0ed2fdbcec70801a05b4a1f
MatteoFasulo <mat.fasulo@gmail.com>
2025-04-24 20:26:33 +0000

    Refactor output path for CUDA performance results and clean up test script

--- test_cuda.sh
+++ test_cuda.sh
@@ -2,9 +2,8 @@
 # run-cuda-program.sh
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --mem=1
 #SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task 2
+#SBATCH --cpus-per-task 4
 #SBATCH --time=1-01:00:00
 #SBATCH --output slurm-%j.out
 #SBATCH --partition=rtx2080
@@ -14,11 +13,13 @@ nvcc cuda-multi-layer-nn.cu -o cuda-multi-layer-nn
 echo "== Test with CUDA =="
 ./cuda-multi-layer-nn 65536 1000
 ./cuda-multi-layer-nn 65536 1000
-./cuda-multi-layer-nn 65536 1000
+
 ./cuda-multi-layer-nn 131072 1000
+
 ./cuda-multi-layer-nn 262144 1000
+
 ./cuda-multi-layer-nn 524288 1000
-./cuda-multi-layer-nn 1048576 1000
+
 ./cuda-multi-layer-nn 1048576 1000
 ./cuda-multi-layer-nn 1048576 1000
 echo "== End of Job =="
